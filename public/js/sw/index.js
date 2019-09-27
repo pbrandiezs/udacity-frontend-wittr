769bd965ad8b086d20c2956ea876311ea6a4aceb
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-26 17:53:51 -0700

    fix: removed a console.log

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,6 @@
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  console.log(event.request.url.split('.')[1]);
   if (event.request.url.endsWith('.jpg')) {
     event.respondWith(
       fetch('/imgs/dr-evil.gif')
