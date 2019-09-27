7acf87829c044076307034ec9b48cd656dc6210e
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-26 17:25:31 -0700

    feat: Modified response

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,11 @@
 self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  // TODO: respond to all requests with an html response
+  // containing an element with class="a-winner-is-me".
+  // Ensure the Content-Type of the response is "text/html"
+  //console.log(event.request);
+  event.respondWith(
+    new Response('<b class="a-winner-is-me">Hello!</b>', {
+        headers: {'Content-Type': 'text/html; charset=utf-8'}
+    })
+  );
 });
