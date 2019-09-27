22959b315462192291dd1850310bd5feb5883917
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-26 17:52:35 -0700

    fix: Implemented intercept with request.url.endsWith

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,8 +2,7 @@ self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
   console.log(event.request.url.split('.')[1]);
-  if ((event.request.url.split('.')[1]) === "jpg") {
-    console.log("It's a jpg!");
+  if (event.request.url.endsWith('.jpg')) {
     event.respondWith(
       fetch('/imgs/dr-evil.gif')
     );
