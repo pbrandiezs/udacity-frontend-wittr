b016a038a73201d9f6a9fbc3812c802ef2f78322
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-27 12:06:43 -0700

    feat: Implemented course example solution.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -13,39 +13,11 @@ self.addEventListener('install', function(event) {
   );
 });
 
-//self.addEventListener('fetch', function(event) {
-  // TODO: respond with an entry from the cache if there is one.
-  // If there isn't, fetch from the network.
-// });
-
 self.addEventListener('fetch', function(event) {
-  console.log('Handling fetch event for', event.request.url);
-
   event.respondWith(
-
-    // Opens Cache objects that start with 'font'.
-    caches.open("wittr-static-v1").then(function(cache) {
-      return cache.match(event.request).then(function(response) {
-        if (response) {
-          console.log('Found response in cache:', response);
-
-          return response;
-        }
-
-        console.log('Fetching request from the network');
-
-        return fetch(event.request).then(function(networkResponse) {
-          cache.put(event.request, networkResponse.clone());
-
-          return networkResponse;
-        });
-      }).catch(function(error) {
-
-        // Handles exceptions that arise from match() or fetch().
-        console.error('Error in fetch handler:', error);
-
-        throw error;
-      });
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
     })
   );
 });
