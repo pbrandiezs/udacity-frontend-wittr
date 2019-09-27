9421ad7abf9f80719735539f0ce926fc7b9f2eeb
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-26 17:48:15 -0700

    feat: Intercept jpg's.

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,7 +1,11 @@
 self.addEventListener('fetch', function(event) {
   // TODO: only respond to requests with a
   // url ending in ".jpg"
-  event.respondWith(
-    fetch('/imgs/dr-evil.gif')
-  );
+  console.log(event.request.url.split('.')[1]);
+  if ((event.request.url.split('.')[1]) === "jpg") {
+    console.log("It's a jpg!");
+    event.respondWith(
+      fetch('/imgs/dr-evil.gif')
+    );
+  }
 });
