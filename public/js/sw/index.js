0d25bdeaaeb478bd899ea150f0bbfaa047576f64
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-27 16:33:58 -0700

    feat: Implemented exercise solution

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,8 +1,13 @@
-var staticCacheName = 'wittr-static-v2';
-
+// test comment change
+//
+//
+//
 self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
+    // TODO: change the site's theme, eg swap the vars in public/scss/_theme.scss
+    // Ensure at least $primary-color changes
+    // TODO: change cache name to 'wittr-static-v2'
+    caches.open('wittr-static-v2').then(function(cache) {
       return cache.addAll([
         '/',
         'js/main.js',
@@ -17,16 +22,8 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName.startsWith('wittr-') &&
-                 cacheName != staticCacheName;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
-      );
-    })
+    // TODO: remove the old cache
+    caches.delete('wittr-static-v1')
   );
 });
 
